745651c6cdfe63d335bfdb0f4479956b1c684818
Keigo Nitadori <keigo@riken.jp>
2021-03-12 15:24:52 +0900

    new formulation

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <x86intrin.h>
 
+#if 0
 void rsqrtCubed_avx512(const double * __restrict x, double * __restrict y, const int N){
 	const __m512d c1 = _mm512_set1_pd(3./2.);
 	for(int i=0; i<N; i+=8){
@@ -23,6 +24,56 @@ void rsqrtCubed_avx512(const double * __restrict x, double * __restrict y, const
 		_mm512_storeu_pd(y+i, ri3p);
 	}
 }
+#else
+// #define RSQRTCUBED_MINLAT
+void rsqrtCubed_avx512(const double * __restrict xs, double * __restrict ys, const int N){
+	/*
+	 * x : r^2
+	 * y : r^-1
+	 * z : r^-3
+	 * z1 := z0 * (1 + ah + bh^2), where
+	 *   a = 3/2
+	 *   b = 15/8
+	 *   h = 1 - x * y0^2
+	 * z1 = z0 + (z0 * h) * (a + b * h)
+	 * a+b*h = (a+b) - (b*x)*y0^2, for minlat mode
+	 */
+
+	const __m512d one  = _mm512_set1_pd(1.0);
+	const __m512d b    = _mm512_set1_pd(15./8.);
+#ifdef RSQRTCUBED_MINLAT
+	const __m512d apb  = _mm512_set1_pd(27./8.);
+#else
+	const __m512d a    = _mm512_set1_pd( 3./2.);
+#endif
+
+	for(int i=0; i<N; i+=8){
+		__m512d x   = _mm512_loadu_pd(xs + i);
+
+#ifdef RSQRTCUBED_MINLAT
+		__m512d bx  = _mm512_mul_pd(b, x);
+#endif
+
+		__m512d y   = _mm512_rsqrt14_pd(x);
+		
+		__m512d y2  = _mm512_mul_pd(y, y);
+
+		__m512d z   = _mm512_mul_pd(y, y2);
+		__m512d h   = _mm512_fnmadd_pd(x,  y2, one); // c - a * b
+#ifdef RSQRTCUBED_MINLAT
+		__m512d abh = _mm512_fnmadd_pd(bx, y2, apb); // c - a * b
+#else
+		__m512d abh = _mm512_fmadd_pd(b, h, a); // a + b*h
+#endif
+
+		__m512d zh  = _mm512_mul_pd(z, h);
+
+		__m512d zc  = _mm512_fmadd_pd(zh, abh, z);
+
+		_mm512_storeu_pd(ys + i, zc);
+	}
+}
+#endif
 
 #include<cstdio>
 #include<cstdlib>
@@ -34,7 +85,7 @@ int main(){
 		N = 1000,
 	};
 
-	static double x[N], y0[N], y1[N], err[N];
+	static double x[N], y0[N], y1[N], err[N] __attribute__((aligned(256)));
 
 	srand48(20210309);
 	for(int i=0; i<N; i++){
@@ -55,10 +106,12 @@ int main(){
 		printf("%e %e %e %e\n", x[i], y0[i], y1[i], err[i]);
 	}
 
+#if 0
 	std::sort(err, err+N);
 	for(int i=0; i<N; i++){
 		printf("%f %e\n", i * (1.0/N), err[i]);
 	}
+#endif
 
 	return 0;
 }
