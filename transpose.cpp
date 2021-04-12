c7cfe4f601eaedeaba65ad12241dbae741803ad3
Keigo Nitadori <keigo@riken.jp>
2021-04-12 22:50:58 +0900

    avx512 3-word transpose

---
+++ transpose.cpp
@@ -0,0 +1,76 @@
+#include <cstdio>
+#include <cassert>
+#include <x86intrin.h>
+
+int main(){
+	float SoA[ 3][16];
+	float AoS[16][ 3];
+
+	for(int i=0; i<16; i++){
+		for(int c=0; c<3; c++){
+			SoA[c][i] = (100*(c+1)) + (i+1);
+			AoS[i][c] = -1.0;
+		}
+	}
+
+	// make [x0, y0, x1, y1,...,x7, y7]
+	int ixylo[16] = { 0, 16,  1, 17,  2, 18,  3, 19,  4, 20,  5, 21,  6, 22,  7, 23};
+
+	// make [x8, y8, x1, y1,...,x15, y15]
+	int ixyhi[16] = { 8, 24,  9, 25, 10, 26, 11, 27, 12, 28, 13, 29, 14, 30, 15, 31};
+
+	// align to make [x5,y5,...,x12,y12]
+	
+	// make [x0,y0,z0,...,x5]
+	int ixyzlow[16] = {0, 1, 16,    2,3,17,    4,5,18,    6,7,19,    8,9,20,    10};
+
+	// make [y5,z5, ... , x10,y10]
+	int ixyzmid[16] = {3,21,    4,5,22,    6,7,23,    8,9,24,    10,11,25,    12,13};
+
+	// make [z10, ... , x15,y15,a15]
+	int ixyzhig[16] = {26,    6,7,27,    8,9,28,    10,11,29,    12,13,30,    14,15,31};
+
+	__m512i xv = _mm512_loadu_epi32(SoA[0]);
+	__m512i yv = _mm512_loadu_epi32(SoA[1]);
+	__m512i zv = _mm512_loadu_epi32(SoA[2]);
+
+	__m512i mxylo = _mm512_loadu_epi32(ixylo);
+	__m512i mxyhi = _mm512_loadu_epi32(ixyhi);
+	__m512i mxyzlow = _mm512_loadu_epi32(ixyzlow);
+	__m512i mxyzmid = _mm512_loadu_epi32(ixyzmid);
+	__m512i mxyzhig = _mm512_loadu_epi32(ixyzhig);
+
+	__m512i xylow = _mm512_permutex2var_epi32(xv, mxylo, yv);
+	__m512i xyhig = _mm512_permutex2var_epi32(xv, mxyhi, yv);
+	__m512i xymid =_mm512_alignr_epi32 (xyhig, xylow, 8);
+
+	__m512i xyzlow = _mm512_permutex2var_epi32(xylow, mxyzlow, zv);
+	__m512i xyzmid = _mm512_permutex2var_epi32(xymid, mxyzmid, zv);
+	__m512i xyzhig = _mm512_permutex2var_epi32(xyhig, mxyzhig, zv);
+
+	_mm512_storeu_epi32(AoS[0]+ 0, xyzlow);
+	_mm512_storeu_epi32(AoS[0]+16, xyzmid);
+	_mm512_storeu_epi32(AoS[0]+32, xyzhig);
+
+	for(int i=0; i<16; i++){
+		for(int c=0; c<3; c++){
+			printf("%3.0f ", AoS[i][c]);
+		}
+		puts("");
+	}
+	for(int c=0; c<3; c++){
+		for(int i=0; i<16; i++){
+			printf("%3.0f ", AoS[0][16*c+i]);
+			// assert(AoS[i][c] == SoA[i][c]);
+		}
+		puts("");
+	}
+	fflush(stdout);
+	for(int i=0; i<16; i++){
+		for(int c=0; c<3; c++){
+			assert(AoS[i][c] == SoA[c][i]);
+		}
+	}
+
+	return 0;
+}
