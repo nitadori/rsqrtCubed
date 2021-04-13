0d472ba17af571de4d3809bd1f43f43c87ad87d7
Keigo Nitadori <keigo@riken.jp>
2021-04-13 12:15:34 +0900

    epi32 -> ps

--- transpose.cpp
+++ transpose.cpp
@@ -15,9 +15,9 @@ int main(){
 
 	asm volatile ("#BEG");
 
-	__m512i xv = _mm512_loadu_epi32(SoA[0]);
-	__m512i yv = _mm512_loadu_epi32(SoA[1]);
-	__m512i zv = _mm512_loadu_epi32(SoA[2]);
+	__m512 xv = _mm512_loadu_ps(SoA[0]);
+	__m512 yv = _mm512_loadu_ps(SoA[1]);
+	__m512 zv = _mm512_loadu_ps(SoA[2]);
 
 #if 0
 	// make [x0, y0, x1, y1,...,x7, y7]
@@ -51,23 +51,23 @@ int main(){
 	__m512i xyzmid = _mm512_permutex2var_epi32(xymid, mxyzmid, zv);
 	__m512i xyzhig = _mm512_permutex2var_epi32(xyhig, mxyzhig, zv);
 #else
-	__m512i xylow = _mm512_shuffle_i32x4 (xv, yv, 0b01000100); // 1010(4)
-	__m512i xymid = _mm512_shuffle_i32x4 (xv, yv, 0b10011001); // 2121(4)
-	__m512i xyhig = _mm512_shuffle_i32x4 (xv, yv, 0b11101110); // 3232(4)
+	__m512 xylow = _mm512_shuffle_f32x4 (xv, yv, 0b01000100); // 1010(4)
+	__m512 xymid = _mm512_shuffle_f32x4 (xv, yv, 0b10011001); // 2121(4)
+	__m512 xyhig = _mm512_shuffle_f32x4 (xv, yv, 0b11101110); // 3232(4)
 
 	//                              | x, y, z | x, y, z | x, y, z | x, y, z | x, y, z | x, y, z |
 	__m512i ilow = _mm512_setr_epi32( 0, 8,16,  1, 9,17,  2,10,18,  3,11,19,  4,12,20,  5      );
 	__m512i imid = _mm512_setr_epi32(    9,21,  2,10,22,  3,11,23,  4,12,24,  5,13,25,  6,14   );
 	__m512i ihig = _mm512_setr_epi32(      26,  3,11,27,  4,12,28,  5,13,29,  6,14,30,  7,15,31);
 
-	__m512i xyzlow = _mm512_permutex2var_epi32(xylow, ilow, zv);
-	__m512i xyzmid = _mm512_permutex2var_epi32(xymid, imid, zv);
-	__m512i xyzhig = _mm512_permutex2var_epi32(xyhig, ihig, zv);
+	__m512 xyzlow = _mm512_permutex2var_ps(xylow, ilow, zv);
+	__m512 xyzmid = _mm512_permutex2var_ps(xymid, imid, zv);
+	__m512 xyzhig = _mm512_permutex2var_ps(xyhig, ihig, zv);
 #endif
 
-	_mm512_storeu_epi32(AoS[0]+ 0, xyzlow);
-	_mm512_storeu_epi32(AoS[0]+16, xyzmid);
-	_mm512_storeu_epi32(AoS[0]+32, xyzhig);
+	_mm512_storeu_ps(AoS[0]+ 0, xyzlow);
+	_mm512_storeu_ps(AoS[0]+16, xyzmid);
+	_mm512_storeu_ps(AoS[0]+32, xyzhig);
 
 	asm volatile ("#END");
 
